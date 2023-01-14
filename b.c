ba90b816706310ae53b03f7a7f3eaa0fceee62e6
zhonglihua <fridy@outlook.com>
2023-01-15 03:58:32 +0800

    modified in vscode , see tab size

--- b.c
+++ b.c
@@ -1,1 +1,7 @@
-remote file
+#include "stdio.h"
+
+int mian(int argc, const char* argv[])                  //5tab
+{                                                       //14tab
+    printf("hello world \r\n");                         //7tab
+    return 0;                                           //10tab + 4 space
+}
