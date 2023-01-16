1195c9fcf6cee2e71623719a097bdd70fb5efe45
zhonglihua <fridy@outlook.com>
2023-01-16 17:00:11 +0800

    update a.c

--- a.c
+++ a.c
@@ -1,11 +1,1 @@
-#include "stdio.h"
-
-int main(  int argc, const char *argv[] )								 # 8tab+1space
-
-{																							# 23tab
-
-​			printf("hello world");												  # 10tab+2space
-
-}																							#23tab
-
-​																							 #23tab +1 sapce
+hello
